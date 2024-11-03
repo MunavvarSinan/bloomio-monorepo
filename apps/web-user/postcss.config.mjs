975093e7c6bc294291a203f20b720da14ed845d6
MunavvarSinan <munavvarsinan8@gmail.com>
2024-11-04 01:09:52 +0530

    fix: postcss error

--- apps/web-user/postcss.config.mjs
+++ apps/web-user/postcss.config.mjs
@@ -1,2 +1,8 @@
-import postcssConfig from "@repo/tailwind-config/postcss.config";
-export default postcssConfig;
+/** @type {import('postcss-load-config').Config} */
+const config = {
+    plugins: {
+        tailwindcss: {},
+    },
+};
+
+export default config;
